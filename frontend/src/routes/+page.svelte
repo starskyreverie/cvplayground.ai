3b63da0cdfc7d1460aaa6065a94ab1aa2d6f3ba5
Nikhil Mehta <nikmehta@nvidia.com>
2024-03-28 05:51:31 -0700

    chore: intro

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts"></script>
+
+<svelte:head>
+	<title>CV Playground</title>
+</svelte:head>
+
+<h2>Computer Vision Playground</h2>
+<div>
+	Still a work in progress. I'm building a web app that will let you play with computer vision
+	models and state-of-the-art research techniques. If you want to contribute, check out the 
+    <a href="https://github.com/pq43/cvplayground">GitHub</a>!
+</div>
+
+<style>
+	:global(body) {
+		font-family: 'Inter', sans-serif;
+	}
+</style>

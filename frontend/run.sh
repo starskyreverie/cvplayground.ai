8bab709229234b4abc5df4f5c6e80c8fe4c07eeb
Nikhil Mehta <nikmehta@nvidia.com>
2024-03-28 01:01:52 -0700

    chore: revert to sveltekit

--- frontend/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Build the project initially.
-wasm-pack build
-
-# Start the live-server to serve the pkg directory.
-live-server pkg/ &
-
-# Watch for changes in the src directory, rebuild the project when changes are detected.
-cargo watch -w src/ -s "wasm-pack build"
